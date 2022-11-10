53cc8f5830fbac4a9cc7914b7d55524ea97de91f
melaster <cedric.DELAITE@student.umons.ac.be>
2022-11-10 14:21:07 +0100

    Q7

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -93,7 +93,9 @@ class ProfCoursTest extends TestCase
             new Cours("Cours7", "3", 5),       // idcours = 7   ** A SUPPRIMER **
             new Cours("Cours8", "4", 5),       // idcours = 8
             new Cours("Cours9", "3", 5),        // idcours = 9   ** A MODIFIER **
-            
+            new Cours("IOT", "10", 1),        //
+	    new Cours("IA", "12", 3),        // 
+	    new Cours("EDL", "5", 6),        // 
             /**
             *
             * Question 7 : Insérer les enregistrements suivantes dans la table cours
